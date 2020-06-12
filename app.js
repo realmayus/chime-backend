9d9f44f72d9524c40c609d50e91e31aa61bfebd2
realmayus <realmayus@gmail.com>
2020-06-12 20:52:42 +0200

    Added youtube search endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fetch = require("node-fetch");
 const admin = require('firebase-admin');
 const cors = require('cors');
 
+
 admin.initializeApp({credential: admin.credential.cert(require("./secret/firebase_creds.json"))});
 
 const app = express();
@@ -15,7 +16,7 @@ app.get('/getProfile', async (request, response) => {
 
     const token = request.query.token
     if(token === null || token === undefined) {
-        response.send("You have to provide a token!");
+        response.status(401).send({errorCode: "invalid-token", error: "You have to provide a valid discord API token!"});
         return;
     }
     let fetchedInfo = await fetch("https://discord.com/api/users/@me", {
@@ -42,7 +43,7 @@ app.get('/getProfile', async (request, response) => {
             return null;
         })
         .catch(err => {
-            response.send("An error occurred: <code>" + err.text + "</code>")
+            response.status(500).send({errorCode: "unknown", error: "An unknown error occurred: " + err});
             console.log(err)
         })
 });
@@ -57,11 +58,11 @@ app.get('/getPlaylist', async (request, response) => {
 
 
     if(token === null || token === undefined) {
-        response.send("You have to provide a token!");
+        response.status(401).send({errorCode: "invalid-token", error: "You have to provide a valid discord API token!"});
         return;
     }
     if(playlist === null || playlist === undefined) {
-        response.send("You have to provide a playlist!");
+        response.status(400).send({errorCode: "invalid-playlist", error: "You have to provide a playlist ID!"});
         return;
     }
 
@@ -79,7 +80,7 @@ app.get('/getPlaylist', async (request, response) => {
         .get()
         .then(doc => {
             if(!doc.exists) {
-                response.status(404).send({"errorCode": "not-existing", "error": "The provided playlist ID doesn't point to an existing playlist."})
+                response.status(404).send({errorCode: "not-existing", error: "The provided playlist ID doesn't point to an existing playlist."})
                 return null;
             }
             let data = doc.data();
@@ -87,9 +88,57 @@ app.get('/getPlaylist', async (request, response) => {
             return null;
         })
         .catch(err => {
-            response.status(500).send("An error occurred: <code>" + err.text + "</code>")
+            response.status(500).send({errorCode: "unknown", error: "An unknown error occurred: " + err});
             console.log(err)
         })
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/getSearchResults", async(request, response) => {
+    response.set('Access-Control-Allow-Origin', "*")
+    response.set('Access-Control-Allow-Methods', 'GET, POST')
+
+    const token = request.query.token
+    const query = request.query.query
+
+    if(token === null || token === undefined) {
+        response.status(401).send({errorCode: "invalid-token", error: "You have to provide a valid discord API token (to prevent abuse)"});
+        return;
+    }
+    if(query === null || query === undefined || query.length === 0) {
+        response.status(400).send({errorCode: "invalid-query", error: "You have to provide a (valid) query!"});
+        return;
+    }
+
+
+
+    let fetchedInfo = await fetch("https://discord.com/api/users/@me", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    fetchedInfo = await fetchedInfo.json();
+    let user_id = fetchedInfo.id;
+    if(user_id) {
+        let results = [];
+        let iterations = 0;
+        do {
+            let fetchedInfo = await fetch("http://youtube-scrape.herokuapp.com/api/search?q=" + encodeURIComponent(query));
+            fetchedInfo = await fetchedInfo.json();
+            results = fetchedInfo.results;
+            iterations++;
+        } while (results.length === 0 && iterations < 5)
+
+        if (results.length > 0) {
+            response.send({results});
+        } else {
+            response.status(404).send({errorCode: "no-results", error: "Couldn't find any results."});
+        }
+
+    } else {
+        response.status(403).send({errorCode: "invalid-token", error: "You have to provide a valid discord API token (to prevent abuse)"});
+        return;
+    }
+})
+
+
+app.listen(port, () => console.log(`Chime backend listening at http://localhost:${port}`))
